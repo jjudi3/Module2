fa53121cc616e5241fb1ed643980ddfee8dc51a1
Judi Perez <j_vperez29@bellsouth.net>
2017-11-09 18:11:02 -0500

    add rest of prework

---
+++ Module-10/javascript.js
@@ -0,0 +1,16 @@
+
+
+<script>
+document.getElementById("Grow").addEventListener("click", function(){
+document.getElementById("box").style.height = "250px";
+});
+document.getElementById("Blue").addEventListener("click", function(){
+document.getElementById("box").style.background-color:blue;
+});
+document.getElementById("Fade").addEventListener("click", function(){
+document.getElementById("box").background-color:(orange, 0.15);
+});
+document.getElementById("Reset").addEventListener("click", function(){
+document.getElementById("box").style.height = "150px";
+});	
+</script>
